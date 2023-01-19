88cd2483f85535396f9b298297ca7acbb8471ea6
Christopher Maltez <cflux.maltez@live.com>
2023-01-18 17:12:57 -0800

    working on pulling it together.

--- src/Board.js
+++ src/Board.js
@@ -28,22 +28,23 @@ import "./Board.css";
  *
  **/
 
-function Board({ nrows, ncols, chanceLightStartsOn }) {
+function Board({ nrows = 6, ncols = 7, chanceLightStartsOn = 0.5 }) {
+  // default params
   const [board, setBoard] = useState(createBoard());
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
   function createBoard() {
-    // TODO: create array-of-arrays of true/false values
+    // // TODO: create array-of-arrays of true/false values
     return new Array(5).fill().map(() =>
       new Array(5).fill().map(() => {
-        return Math.round(Math.random()) ? false : true;
+        return Math.random() < chanceLightStartsOn;
       })
     );
   }
 
   /** check if all values in board matrix are true. */
   function hasWon() {
-    // TODO: check the board in state to determine whether the player has won.
+    // // TODO: check the board in state to determine whether the player has won.
     return _.flattenDeep(board).every(true);
   }
 
@@ -53,29 +54,43 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
 
       const flipCell = (y, x, boardCopy) => {
         // if this coord is actually on board, flip it
-
         if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
           boardCopy[y][x] = !boardCopy[y][x];
         }
       };
 
       // TODO: Make a (deep) copy of the oldBoard
-      let oldBoardCopy = _.cloneDeep(oldBoard);
+      let deepBoardCopy = _.cloneDeep(oldBoard);
 
       // TODO: in the copy, flip this cell and the cells around it
+      // deepBoardCopy.forEach((cell) => flipCell(y, x));
+      let rowAbove = y - 1;
+      let rowBelow = y + 1;
+      let colLeft = x - 1;
+      let colRight = x + 1;
 
-      oldBoardCopy.forEach((cell) => flipCell(y, x));
+      flipCell(y, x, deepBoardCopy); // flip cell clicked on
+      flipCell(y - 1, x, deepBoardCopy); // flip cell above
+      flipCell(y + 1, x, deepBoardCopy); // flilp cell below
+      flipCell(y, x - 1, deepBoardCopy); // flip left cell
+      flipCell(y, x + 1, deepBoardCopy); // flip right cell
+
       // TODO: return the copy
+      return deepBoardCopy;
     });
   }
 
   // if the game is won, just show a winning msg & render nothing else
-
   // TODO
+  if (hasWon()) {
+    return <p>"you have won"</p>;
+  }
 
   // make table board
-
   // TODO
+  return (
+
+  )
 }
 
 export default Board;
